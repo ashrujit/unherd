77f2c642fe96be3fd1afc17fccbda96c64c7c275
Agarwal <agarwalpranaya@gmail.com>
2014-01-26 23:34:24 +0530

    Adding interface to be called from node.

--- lib/engine/test/test.py
+++ lib/engine/test/test.py
@@ -1,18 +1,34 @@
 from .. naive  import tscore
+# from .. interface  import nodeEngine
 
-import nltk
 import json
+import sys
 
-model = tscore.ScoreModel()
+def rawTest():
+	model = tscore.ScoreModel()
+	f = open('engine/test/earth.txt', 'r')
 
-f = open('engine/test/earth.txt', 'r')
+	for line in f.readlines():
+		tweetJSON = json.loads(line)
+		print model.score(line), tweetJSON['text']
 
-for line in f.readlines():
-
-	tweetJSON = json.loads(line)
-
-	print model.score(line), tweetJSON['text']
-
-f.close()
+	f.close()
 
 
+def batchTest():
+	"""
+		Testing the node interface for batch tweet processing.
+	"""
+	tweetJSONArr = []
+	
+	f = open('engine/test/earth.txt', 'r')
+	
+	for line in f.readlines():
+		tweetJSONArr.append(line)
+	f.close()
+	return nodeEngine.scoreBatch(tweetJSONArr)
+
+
+
+# rawTest()
+# batchTest()	
