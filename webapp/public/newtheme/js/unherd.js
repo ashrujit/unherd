2826e0706cde08d152eea4509c708e807486439e
mithunsatheesh <mithunsatheesh@mithunsatheesh.(none)>
2014-03-01 22:46:24 +0530

    fixes #25. js function timeago altered.

--- webapp/public/newtheme/js/unherd.js
+++ webapp/public/newtheme/js/unherd.js
@@ -184,23 +184,27 @@ function FnLoadMyTimeline() {
 function FnTimeAgo(time){
 	
 	var units = [
-	{ name: "second", limit: 60, in_seconds: 1 },
-	{ name: "minute", limit: 3600, in_seconds: 60 },
-	{ name: "hour", limit: 86400, in_seconds: 3600  },
-	{ name: "day", limit: 604800, in_seconds: 86400 },
-	{ name: "week", limit: 2629743, in_seconds: 604800  },
-	{ name: "month", limit: 31556926, in_seconds: 2629743 },
+	{ name: "s", limit: 60, in_seconds: 1 },
+	{ name: "m", limit: 3600, in_seconds: 60 },
+	{ name: "h", limit: 86400, in_seconds: 3600  },
 	{ name: "year", limit: null, in_seconds: 31556926 }
 	];
-	var diff = (new Date() - new Date(time)) / 1000;
+	var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+	var olddate = new Date(time);
+	var diff = (new Date() - olddate) / 1000;
 	if (diff < 5) return "now";
 
 	var i = 0;
 	while (unit = units[i++]) {
-	if (diff < unit.limit || !unit.limit){
-	  var diff =  Math.floor(diff / unit.in_seconds);
-	  return diff + " " + unit.name + (diff>1 ? "s ago" : "");
-	}
+		if (diff < unit.limit || !unit.limit){
+			if(!unit.limit) {
+				return monthNames[olddate.getMonth()]+" "+olddate.getDate();
+			} else {
+				var diff =  Math.floor(diff / unit.in_seconds);
+				return diff + "" + unit.name + (diff>1 ? "" : "");
+			}
+		}
 	};
 }
 
