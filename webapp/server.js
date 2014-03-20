d21a8778eae8f2d0940c25b06db2f93fc2b0a692
ashrujit <ashrujit@gmail.com>
2014-03-19 18:17:59 -0700

    config changes

--- webapp/server.js
+++ webapp/server.js
@@ -409,6 +409,5 @@ app.get('/*', function(req, res){
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.OPENSHIFT_NODEJS_IP||"172.31.47.200";
-var ip = process.env.OPENSHIFT_NODEJS_IP||"127.0.0.1";
 
 app.listen(port,ip);
