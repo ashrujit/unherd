fee6671f1ddf5c4aa43dfe173f53fef41e872ac0
ashrujit <ashrujit@gmail.com>
2014-04-04 10:35:11 +0000

    engine updates from @pranay code 010414

--- lib/engine/naive/ner.py
+++ lib/engine/naive/ner.py
@@ -1,4 +1,4 @@
-import nltk
+import nltk.data, nltk.tag
 
 def getEntities(tweetObj):
 	"""
@@ -9,20 +9,33 @@ def getEntities(tweetObj):
 	entitiesList = []
 	
 	for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(tweet))):
-		# print chunk
 		if hasattr(chunk, 'node'):
-			# print chunk.node, chunk.leaves()
 			entitiesList.append(chunk)
 
 	return entitiesList
 
-def getValue(tweetObj):
+def getValueOld(tweetObj):
 		"""
 			Returns the Value as per decided logic for the list of
 			entities identitied 
 		"""	
 		entitiesList = getEntities(tweetObj)
-		# print entitiesList
 		
 		categoryList = set(chunk.node for chunk in entitiesList)	
 		return len(categoryList)
+
+
+def getValue(tweetObj):
+ 		"""
+ 			Faster way to get the NER from the sentence.
+ 		"""	
+ 		tweet = tweetObj['text']
+ 		words = nltk.word_tokenize('Obama is the president of America')
+ 		tagger = nltk.data.load(nltk.tag._POS_TAGGER)
+ 		chunks = tagger.tag(words)
+ 		score = 0
+ 		for chunk in chunks:
+ 			if(chunk[1] == 'NNP'):
+ 				score += 1
+ 
+ 		return score;
--- lib/engine/test/test.py
+++ lib/engine/test/test.py
@@ -17,10 +17,14 @@ class BasicUnitTests(unittest.TestCase):
     
     def tearDown(self):
         t = time.time() - self.startTime
-        print "%s: %.3f" % (self.id(), t)
-    
-    def testRaw(self):
-        model = tscore.ScoreModel()
+        print "\n%s: %.3f\n" % (self.id(), t)
+
+    def testBasicSlow(self):
+        """
+            Test for the library. It directly calls the python
+            module.
+        """        
+	model = tscore.ScoreModel()
         f = open(self.raw_sample, 'r')
         responseArr = []
 
@@ -35,21 +39,9 @@ class BasicUnitTests(unittest.TestCase):
             print res[0], res[1]
 
 
-    # def testBatchCallToAPI(self):
-    #     """
-    #         Testing the node interface for batch tweet processing.
-    #     """
-    #     tweetJSONArr = []
-        
-    #     f = open(self.api_batch_sample, 'r')
-        
-    #     for line in f.readlines():
-    #         tweetJSONArr.append(line)
-    #     f.close()
-
-    def NOtestAPI(self):
+    def testAPIFast(self):
         """
-            Fire post request to the API.
+		Fire post request to the API. It needs the sever live.
         """
         
 
@@ -57,7 +49,7 @@ class BasicUnitTests(unittest.TestCase):
         
         f = open(self.api_sample, 'r')
         dataArr = []
-        for i in range(20):
+        for i in range(180):
             line = f.readline()
             line = line.replace("\n", "")
             # This works as well
